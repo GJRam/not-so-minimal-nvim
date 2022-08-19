80714a23d2c16a867d139552571f5a89ccaa1533
GJRam <zeac125@live.rhul.ac.uk>
2022-08-19 14:53:00 +0100

    added go to definition

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -2,23 +2,32 @@ local status, nvim_lsp = pcall(require, 'lspconfig')
 
 if (not status) then return end
 local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
   if client.server_capabilities.documentFormattingProvider then
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
     vim.api.nvim_command [[augroup END]]
   end
+
+  local opts = { noremap = true, silent = true }
+
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+
 end
 
+
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
+  vim.lsp.protocol.make_client_capabilities()
 )
 
 nvim_lsp.tsserver.setup {
-    on_attach = on_attach,
-    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-    cmd = { "typescript-language-server", "--stdio" },
-    capabilities = capabilities
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
+  capabilities = capabilities
 }
 
 require('lspconfig')['pyright'].setup {
